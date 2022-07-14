fbfa88495492ac8b1d2dc3a6e95d80db1dfd8682
Artak1999 <dashyan.1999@mail.ru>
2022-07-14 15:53:13 +0500

    Interesting tasks.

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,23 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int p,res = 1;
+        for(int i=1; i<=cathetusLength; i++){
+            p=i;
+            for(int j=1; j<=cathetusLength-i; j++)
+                System.out.print(" ");
+            for(int k=1; k<=2*i-1; k++){
+                System.out.print(p);
+                if(k==i)
+                    res=0;
+                if(res==1)
+                    p--;
+                else
+                    p++;
+            }
+            res=1;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
